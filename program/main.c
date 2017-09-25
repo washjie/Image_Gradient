b424b04a5deab2112d74195ba0fe0a87a6c4041c
王聖傑 <wangshengjie@oukiyoshiketsus-MacBook-Air.local>
2017-09-25 21:36:01 +0800

    main process

--- program/main.c
+++ program/main.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]){
 	
 	if(argc == 2){
 		image_info image = read_image(argv[1]);	
-		image_routine(image);
+		image_routine(image, "g1g2");
 	}
 	else{
 		printf("No arguments.");
