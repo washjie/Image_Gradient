9a5e420d13de154d6c34342b4dbf4fccd58fd386
王聖傑 <wangshengjie@oukiyoshiketsus-MacBook-Air.local>
2017-09-25 21:23:59 +0800

    main program, need one argument (image_file.pgm).

--- program/main.c
+++ program/main.c
@@ -1,12 +1,14 @@
 #include "image_io.h"
 #include "process.h"
 
-/* started here */
 int main(int argc, char* argv[]){
 	
 	if(argc == 2){
 		image_info image = read_image(argv[1]);	
 		image_routine(image);
+	}
+	else{
+		printf("No arguments.");
 	}
 
 	return 0;
